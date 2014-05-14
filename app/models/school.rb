70648c50b582dee0636f9572a1b5416d1dfe92b0
csmooth <christiandsmith1990@gmail.com>
2014-05-14 15:46:01 -0700

    Working home, view and prototype pages.  Prototype just views at this point

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,3 @@
 class School < ActiveRecord::Base
 	has_many :test_scores
-	has_attached_file :spreadsheet
-	validates_attachment :spreadsheet, :content_type => { :content_type => ["text/csv"] }
 end
--- app/models/test_score.rb
+++ app/models/test_score.rb
@@ -1,3 +1,9 @@
 class TestScore < ActiveRecord::Base
 	belongs_to :school
+
+	def initializer(student_id, subject, score)
+		@student_id = student_id
+		@subject = subject
+		@score = score
+	end
 end
